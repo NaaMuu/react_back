98714da8b02a8bbfb34cce8df4ab61d774ae033c
NaaMuu <kk0506014@gmail.com>
2024-09-09 16:43:09 +0900

    로그인 추가 중

--- react_back/server.js
+++ react_back/server.js
@@ -24,9 +24,32 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/api/fetch_post', (req, res) => {
-  const query = "SELECT * FROM posts ORDER BY num DESC";
+  const query = "SELECT * FROM post ORDER BY num DESC";
   pool.query(query, (err, rows, fields) => {
     res.send(rows);
+  });
+});
+
+// app.get('/api/fetch_post/:num', (req, res) => {
+//   const num = req.params.num;
+//   const query = 'SELECT * FROM post WHERE num = ?';
+//   pool.query(query, [num], (err, rows, fields) => {
+//       res.json(rows[0]);
+//   });
+// });
+
+app.get("/api/login", (req, res) => {
+  const { id, pw } = req.query;
+  const query = "SELECT * FROM user WHERE id = ? AND pw = ?";
+  pool.query(query, [id, pw], (err, results) => {
+    if (err) {
+      return res.status(500).send("서버 오류 발생");
+    }
+    if (results.length > 0) {
+      res.send({ success: true, message: "로그인 성공", user: results[0] });
+    } else {
+      res.send({ success: false, message: "로그인 실패: ID 또는 비밀번호가 일치하지 않습니다." });
+    }
   });
 });
 
